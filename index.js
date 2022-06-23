13e5c47b9000c41f78d899a6e36ad0df03c7fcb5
Ezeh Victoria <ezehvictoria86@gmail.com>
2022-06-23 15:50:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var name = "Ezeh Victoria<br/>"; 
+    height = "5ft7<br/>"; 
+    country= "Nigeria";
+
+let result = name + height + country
+
+
+document.write("<H2>" + result)
+
+console.log("Name: Ezeh Victoria" + "\n" + "Height: 5ft7" + "\n" + "Country: Nigeria")
+
+window.alert("Name: Ezeh Victoria" + "\n" + "Height: 5ft7" + "\n" + "Country: Nigeria")
+
